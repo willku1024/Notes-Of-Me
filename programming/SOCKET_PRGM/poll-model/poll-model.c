7898d8835185b15f81d340f1faa3df7d2b05011f
willxin <kuwill@willkuMBP.lan>
2019-04-03 16:22:58 +0800

    2019-04-03 16:22:57

--- programming/SOCKET_PRGM/poll-model/poll-model.c
+++ programming/SOCKET_PRGM/poll-model/poll-model.c
@@ -1,3 +1,27 @@
+/*
+IO多路转接 - poll
+poll结构体: 
+struct pollfd {
+int fd; //文件描述符
+short events;  // 需要监听的事件
+short revents; // 内核给的反馈，实际发生的事件
+};
+
+函数原型:
+- int select(int nfds,
+fd_set *readfds, fd_set *writefds,
+fd_set *exceptfds,
+struct timeval *timeout);
+- int poll(struct pollfd *fd, nfds_t nfds, int timeout);
+○ pollfd -- 数组的地址
+○ nfds: 数组的最大长度, 数组中最后一个使用的元素下标+1
+     内核会轮询检测fd数组的每个文件描述符 ○ timeout:
+     -1: 永久阻塞
+     0: 调用完成立即返回  >0: 等待的时长毫秒
+○ 返回值: iO发送变化的文件描述符的个数
+
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -5,15 +29,18 @@
 #include <string.h>
 #include <poll.h>
 #include <netinet/in.h>
-#include <netinet/ip.h> /* superset of previous */
+#include <netinet/ip.h>
 #include <sys/select.h>
 #include <arpa/inet.h>
 #include <errno.h>
 
 #define LISTEN_BACKLOG 50
-#define handle_error(msg) \
-           do { perror(msg); exit(EXIT_FAILURE); } while (0)
-
+#define handle_error(msg)   \
+    do                      \
+    {                       \
+        perror(msg);        \
+        exit(EXIT_FAILURE); \
+    } while (0)
 
 //设置socket非阻塞
 void active_nonblock(int fd)
@@ -22,7 +49,6 @@ void active_nonblock(int fd)
     int flags = fcntl(fd, F_GETFL, 0);
     if (flags == -1)
         handle_error("fcntl");
-
 
     flags |= O_NONBLOCK;
     ret = fcntl(fd, F_SETFL, flags);
@@ -37,7 +63,6 @@ void deactive_nonblock(int fd)
     int flags = fcntl(fd, F_GETFL, 0);
     if (flags == -1)
         handle_error("de-fcntl");
-
 
     flags &= ~O_NONBLOCK;
     ret = fcntl(fd, F_SETFL, flags);
@@ -62,7 +87,6 @@ int main(int argc, char *argv[])
     my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     my_addr.sin_port = htons(atoi(argv[1]));
 
-
     // create
     sfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sfd == -1)
@@ -71,13 +95,11 @@ int main(int argc, char *argv[])
     int on = 1;
     setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 
-    if (bind(sfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr_in)) == -1)
+    if (bind(sfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in)) == -1)
         handle_error("bind");
 
     if (listen(sfd, LISTEN_BACKLOG) == -1)
         handle_error("listen");
-
-
 
     //struct pollfd {
     //    int   fd;         /* file descriptor */
@@ -86,7 +108,6 @@ int main(int argc, char *argv[])
     //};
 
     struct pollfd allfd[1024];
-
 
     for (int j = 0; j < 1024; ++j)
     {
@@ -97,12 +118,10 @@ int main(int argc, char *argv[])
     allfd[0].fd = sfd;
     int maxfd_idx = 0;
 
-
     while (1)
     {
         // int poll(struct pollfd *fds, nfds_t nfds, int timeout);
         // timewait: -1:block 0:call return >0:set timeout
-
 
         puts("poll wait:");
         int retval = poll(allfd, maxfd_idx + 1, -1);
@@ -113,14 +132,13 @@ int main(int argc, char *argv[])
             break;
         }
 
-
         if (allfd[0].revents & POLLIN)
         {
             puts("listen active");
             struct sockaddr_in peer_addr;
             socklen_t peer_addr_size;
             peer_addr_size = sizeof(struct sockaddr_in);
-            cfd = accept(sfd, (struct sockaddr *) &peer_addr, &peer_addr_size);
+            cfd = accept(sfd, (struct sockaddr *)&peer_addr, &peer_addr_size);
 
             if (cfd == -1)
                 handle_error("accept");
@@ -130,7 +148,6 @@ int main(int argc, char *argv[])
                    inet_ntop(AF_INET, &peer_addr.sin_addr, ip, sizeof(ip)), ntohs(peer_addr.sin_port));
 
             active_nonblock(cfd);
-
 
             int i = 1;
             // cfd添加到ｐｏｌｌ数组
@@ -143,10 +160,8 @@ int main(int argc, char *argv[])
                 }
             }
 
-
             // update max fd
             maxfd_idx = maxfd_idx > i ? maxfd_idx : i;
-
         }
 
         for (int i = 1; i < maxfd_idx + 1; ++i)
@@ -165,8 +180,8 @@ int main(int argc, char *argv[])
                     if (n > 0)
                     {
                         printf("%s", buf);
-
-                    } else
+                    }
+                    else
                     {
                         // no data can read
                         if (errno == EAGAIN || errno == EWOULDBLOCK)
@@ -174,7 +189,6 @@ int main(int argc, char *argv[])
                         else
                             puts("read error");
                     }
-
                 }
 
                 if (n == 0)
@@ -188,12 +202,9 @@ int main(int argc, char *argv[])
                 // process some things
                 char sendbuf[] = "srever send data";
                 write(fd, sendbuf, sizeof(sendbuf));
-
             }
         }
-
     }
-
 
     return 0;
 }
--- programming/SOCKET_PRGM/select-model/select.c
+++ programming/SOCKET_PRGM/select-model/select.c
@@ -1,3 +1,31 @@
+/*
+settitimer()
+函数原型:
+int select(int nfds,fd_set *writefds, fd_set *exceptfds,
+struct timeval *timeout); - 参数:
+○ nfds: 要检测的文件描述中最大的fd+1 - 1024
+○ readfds: 读集合
+○ writefds: 写集合
+○ exceptfds: 异常集合
+○ timeout:
+     NULL: 永久阻塞
+    □ 当检测到fd变化的时候返回  struct timeval a;
+    □ a.tv_sec = 10; □ a.tv_usec = 0;
+
+- 全部清空
+○ void FD_ZERO(fd_set *set);
+- 从集合中删除某一项
+○ void FD_CLR(int fd, fd_set *set);
+- 将某个文件描述符添加到集合
+○ void FD_SET(int fd, fd_set *set);
+- 判断某个文件描述符是否在集合中 ○ int FD_ISSET(int fd, fd_set *set);
+使用select函的优缺点: - 优点:
+○ 跨平台 - 缺点:
+○ 每次调用select，都需要把fd集合从用户态拷贝 到内核态，这个开销在fd很多时会很大
+○ 同时每次调用select都需要在内核遍历传递进来 的所有fd，这个开销在fd很多时也很大
+○ select支持的文件描述符数量太小了，默认是 1024
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -11,9 +39,12 @@
 #include <errno.h>
 
 #define LISTEN_BACKLOG 50
-#define handle_error(msg) \
-           do { perror(msg); exit(EXIT_FAILURE); } while (0)
-
+#define handle_error(msg)   \
+    do                      \
+    {                       \
+        perror(msg);        \
+        exit(EXIT_FAILURE); \
+    } while (0)
 
 //设置socket非阻塞
 void active_nonblock(int fd)
@@ -22,7 +53,6 @@ void active_nonblock(int fd)
     int flags = fcntl(fd, F_GETFL, 0);
     if (flags == -1)
         handle_error("fcntl");
-
 
     flags |= O_NONBLOCK;
     ret = fcntl(fd, F_SETFL, flags);
@@ -37,7 +67,6 @@ void deactive_nonblock(int fd)
     int flags = fcntl(fd, F_GETFL, 0);
     if (flags == -1)
         handle_error("de-fcntl");
-
 
     flags &= ~O_NONBLOCK;
     ret = fcntl(fd, F_SETFL, flags);
@@ -62,7 +91,6 @@ int main(int argc, char *argv[])
     my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     my_addr.sin_port = htons(atoi(argv[1]));
 
-
     // create
     sfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sfd == -1)
@@ -71,22 +99,19 @@ int main(int argc, char *argv[])
     int on = 1;
     setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 
-    if (bind(sfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr_in)) == -1)
+    if (bind(sfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in)) == -1)
         handle_error("bind");
 
     if (listen(sfd, LISTEN_BACKLOG) == -1)
         handle_error("listen");
 
-
     fd_set rfds, tmpfds;
 
     FD_ZERO(&rfds);
 
-
     FD_SET(sfd, &rfds);
 
     int maxfd = sfd;
-
 
     while (1)
     {
@@ -98,7 +123,8 @@ int main(int argc, char *argv[])
         {
             perror("select()");
             break;
-        } else if (retval)
+        }
+        else if (retval)
         {
 
             if (FD_ISSET(sfd, &tmpfds))
@@ -107,7 +133,7 @@ int main(int argc, char *argv[])
                 struct sockaddr_in peer_addr;
                 socklen_t peer_addr_size;
                 peer_addr_size = sizeof(struct sockaddr_in);
-                cfd = accept(sfd, (struct sockaddr *) &peer_addr, &peer_addr_size);
+                cfd = accept(sfd, (struct sockaddr *)&peer_addr, &peer_addr_size);
 
                 if (cfd == -1)
                     handle_error("accept");
@@ -120,7 +146,6 @@ int main(int argc, char *argv[])
                 FD_SET(cfd, &rfds);
                 // update max fd
                 maxfd = maxfd > cfd ? maxfd : cfd;
-
             }
 
             for (int i = sfd + 1; i < maxfd + 1; ++i)
@@ -139,12 +164,11 @@ int main(int argc, char *argv[])
                         else
                         {
                             // no data can read
-                            if(errno == EAGAIN || errno == EWOULDBLOCK)
+                            if (errno == EAGAIN || errno == EWOULDBLOCK)
                                 break;
                             else
                                 puts("read error");
                         }
-
                     }
 
                     if (n == 0)
@@ -156,17 +180,13 @@ int main(int argc, char *argv[])
                     }
                 }
             }
-
-
-        } else
+        }
+        else
         {
             // printf("No data within five seconds.\n");
             continue;
         }
-
-
     }
-
 
     return 0;
 }
